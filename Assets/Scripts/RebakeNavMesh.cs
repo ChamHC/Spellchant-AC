d17c04aaf386a8a7367b03de67d4b5075e050a7f
ChamHC <chcham2207@gmail.com>
2024-03-15 14:33:45 +0800

    Added Realtime NavMesh Baking

--- Assets/Scripts/RebakeNavMesh.cs
+++ Assets/Scripts/RebakeNavMesh.cs
@@ -2,18 +2,36 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.AI.Navigation;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class RebakeNavMesh : MonoBehaviour
 {
-    public NavMeshSurface NavMeshSurface;
+    [SerializeField, ReadOnly] public NavMeshSurface NavMeshSurface;
+    public GameObject origin;
+    public float rebuildRadius = 200f;
 
-    private void Start()
+    private NavMeshData navMeshData;
+
+    void Start()
     {
         NavMeshSurface = GetComponent<NavMeshSurface>();
+        navMeshData = new NavMeshData();
+        navMeshData = NavMeshSurface.navMeshData;
+        StartCoroutine(RebuildNavMesh());
     }
 
-    public void Rebake()
+    IEnumerator RebuildNavMesh()
     {
-        NavMeshSurface.BuildNavMesh();
+        while (true)
+        {
+            var sources = new List<NavMeshBuildSource>();
+            var markups = new List<NavMeshBuildMarkup>();
+            var bounds = new Bounds(origin.transform.position, Vector3.one * rebuildRadius); 
+
+            NavMeshBuilder.CollectSources(bounds, NavMeshSurface.layerMask, NavMeshSurface.useGeometry, NavMeshSurface.defaultArea, markups, sources);
+            NavMeshBuilder.UpdateNavMeshDataAsync(navMeshData, NavMeshSurface.GetBuildSettings(), sources, bounds);
+
+            yield return new WaitForSeconds(0.5f);
+        }
     }
 }
